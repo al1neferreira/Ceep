3fcc7997151d549e4d1de35d7c9dd30f78490b56
al1neferreira <alinfst@gmail.com>
2025-01-30 19:15:53 -0300

    Add converter for retrofit

--- app/src/main/java/br/com/alura/ceep/webclient/RetrofitInitializer.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/RetrofitInitializer.kt
@@ -2,13 +2,14 @@ package br.com.alura.ceep.webclient
 
 import br.com.alura.ceep.service.NotesService
 import retrofit2.Retrofit
-
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 class RetrofitInitializer {
 
     var retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://http://localhost:8080/")
+        .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
     val notesService = retrofit.create(NotesService::class.java)
